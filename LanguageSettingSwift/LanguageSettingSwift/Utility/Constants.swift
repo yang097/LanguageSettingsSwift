eda1a1cc8312d4764587df6c32b2cc0ee7d8e3bf
yang097 <99160821+yang097@users.noreply.github.com>
2022-06-15 20:39:09 +1000

    Add files via upload

---
+++ LanguageSettingSwift/LanguageSettingSwift/Utility/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  LanguageSettingSwift
+//
+//  Created by Yang on 14/6/2022.
+//
+
+import Foundation
+
+let texts = ["Hello", "Goodbye", "Yes", "No" , "Thank you", "Love", "Good morning", "Good evening"]
